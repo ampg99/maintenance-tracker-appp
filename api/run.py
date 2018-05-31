206cf06f7539bbd310034202aca55b3c1d9408f3
asheuh <asheuh49@gmail.com>
2018-05-31 13:42:15 +0300

    add changes

--- api/run.py
+++ api/run.py
@@ -10,12 +10,14 @@ def create_app(filename):
     app = Flask(__name__)
     app.config.from_object(filename)
     api = Api(app)
+    
     api.add_resource(RequestsListResource, '/api/v1/requests', endpoint='get_requests')
     api.add_resource(RequestResource, '/api/v1/requests/<int:id>', endpoint='get_one_request')
     api.add_resource(RequestResource, '/api/v1/requests/<int:id>', endpoint='update_request')
     api.add_resource(RequestResource, '/api/v1/requests/<int:id>', endpoint='delete_request')
-    api.add_resource(RequestsListResource, '/api/v1/requests', endpoint='create_request')
+    api.add_resource(RequestsListResource, '/api/v1/requests', '/api/v1/requests/', endpoint='create_request')
     api.add_resource(Testing, '/')
+    
     @app.route('/ping')
     def ping():
         return jsonify(ping='pong')
