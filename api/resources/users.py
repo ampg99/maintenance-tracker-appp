b104b3c0201d9f9ef9e73662169a8e69d4090200
Brian Mboya <asheuh49@gmail.com>
2018-06-02 08:10:10 +0300

    Update users.py

--- api/resources/users.py
+++ api/resources/users.py
@@ -4,28 +4,6 @@ from flask import Flask, render_template, flash, request, url_for, redirect, ses
 from passlib.hash import sha256_crypt
 
 USERS = []
-
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    error = None
-    if request.method == 'POST':
-        if request.form['username'] != app.config['USERNAME']:
-            error = 'Invalid username'
-        elif request.form['password'] != app.config['PASSWORD']:
-            error = 'Invalid password'
-        else:
-            session['logged_in'] = True
-            flash('You were logged in')
-            return redirect(url_for('show_entries'))
-    return render_template('login.html', error=error)
-
-
-@app.route('/logout')
-def logout():
-    session.pop('logged_in', None)
-    flash('You were logged out')
-    return redirect(url_for('show_entries'))
     
 class UsersRegisterResource(Resource):
     def __init__(self):
