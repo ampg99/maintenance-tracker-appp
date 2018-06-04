c1d39ed450c35c576946e38bb1393cf76e0de38f
asheuh <asheuh49@gmail.com>
2018-06-04 06:12:57 +0300

    modify run.py

--- run.py
+++ run.py
@@ -2,8 +2,7 @@ import os
 
 from api import create_app
 
-config_name = os.getenv('APP_SETTINGS') # config_name = "development"
-app = create_app(config_name)
+app = create_app()
 
 if __name__ == '__main__':
     app.run()
